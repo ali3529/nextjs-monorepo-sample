776465d13a60bbbdae0af97a66161d5a26cc7500
alirezaAbbasi <ali3529abbasi@yahoo.com>
2023-10-29 14:10:57 +0330

    Config Nextjs Folder Structure

--- apps/pwa/src/app/page.tsx
+++ apps/pwa/src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Image from "next/image";
-import { Card } from "ui";
+import { Button } from "ui";
 import styles from "./page.module.css";
 
 export default function Page(): JSX.Element {
   return (
     <div className="bg-yellow-600 text-red-500 text-2xl">
       fkjsdakgjhsdjkghsdjkhgj
+      <Button/>
     </div>
   );
 }
---
+++ packages/ui/button.tsx
@@ -0,0 +1,6 @@
+import React, { FC } from "react";
+
+export const Button: FC = () => {
+  return <div>Button</div>;
+};
+Button.displayName="Button"

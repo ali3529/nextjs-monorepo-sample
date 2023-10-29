333c528c263b9bd34ad35f7f845912979be6d9e6
alirezaAbbasi <ali3529abbasi@yahoo.com>
2023-10-29 13:41:25 +0330

    remove default styles

--- apps/pwa/app/page.tsx
+++ apps/pwa/app/page.tsx
@@ -1,60 +1,13 @@
-'use client'
+"use client";
 
 import Image from "next/image";
 import { Card } from "ui";
 import styles from "./page.module.css";
 
-function Gradient({
-  conic,
-  className,
-  small,
-}: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
-}): JSX.Element {
-  return (
-    <span
-      className={[
-        styles.gradient,
-        conic ? styles.glowConic : undefined,
-        small ? styles.gradientSmall : styles.gradientLarge,
-        className,
-      ]
-        .filter(Boolean)
-        .join(" ")}
-    />
-  );
-}
-
-const LINKS = [
-  {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
-  },
-  {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
-  },
-  {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
-  },
-  {
-    title: "Deploy",
-    href: "https://vercel.com/new",
-    description:
-      " Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
-];
-
 export default function Page(): JSX.Element {
   return (
-  <div className="bg-yellow-600 text-red-500 text-2xl">
-fkjsdakgjhsdjkghsdjkhgj
-  </div>
+    <div className="bg-yellow-600 text-red-500 text-2xl">
+      fkjsdakgjhsdjkghsdjkhgj
+    </div>
   );
 }
